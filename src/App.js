f85aaae807f68cad0ecb9a8090dff925f61a71b4
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-29 00:04:36 -0500

    Dependencies installed. Finished logic of function addItem.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
 	const [products] = useState(data);
@@ -13,28 +13,21 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart(item);
 	};
 
 	return (
-		<div className="App">
+		<div className='App'>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
 				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				path='/'
+				render={() => <Products products={products} addItem={addItem} />}
 			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
 		</div>
 	);
 }
