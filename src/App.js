c01da8d63b7c1c8607e8978595a311330f8ded96
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-29 21:55:55 -0500

    Created a removeItem function that removes items from shopping cart. Stretch Goalgit add .

--- src/App.js
+++ src/App.js
@@ -19,9 +19,13 @@ function App() {
 		}
 	};
 
+	const removeItem = itemId => {
+		setCart(cart.filter(item => item.id !== itemId));
+	}
+
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={cart}>
+			<CartContext.Provider value={{ cart, setCart, removeItem }}>
 				<div className='App'>
 					<Navigation cart={cart} />
 					{/* Routes */}
